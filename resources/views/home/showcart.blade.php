3cf6cbaa771506d32a28ab62d88fd66a24841889
Md Masuk Al Hussain <alhussain@iut-dhaka.edu>
2024-08-09 16:43:50 +0600

    moving cart data to order table

--- resources/views/home/showcart.blade.php
+++ resources/views/home/showcart.blade.php
@@ -50,54 +50,68 @@
         <div class="hero_area">
             @include('home.header')
 
+            @if(session()->has('message'))
+                <div class="alert alert-success">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+                    {{session()->get('message')}}
+                </div>
+            @endif
 
-         <div class="center">
-            <table>
-                <tr>
-                    <th class="th_deg">Product Title</th>
-                    <th class="th_deg">Product Quantity</th>
-                    <th class="th_deg">Price</th>
-                    <th class="th_deg">Image</th>
-                    <th class="th_deg">Action</th>
-                </tr>
 
-                <?php $totalPrice = 0; ?>
-
-                @foreach ($cart as $cart)
+            <div class="center">
+                <table>
                     <tr>
-                        <td>{{$cart->product_title}}</td>
-                        <td>{{$cart->quantity}}</td>
-                        <td>${{$cart->price}}</td>
-                        <td><img class="img_deg" src="/product/{{$cart->image}}"></td>
-                        <td><a class="btn btn-danger" onclick="return confirm('Are you sure to remove this product?')" href="{{url('/remove_cart', $cart->id)}}">Remove</a></td>
+                        <th class="th_deg">Product Title</th>
+                        <th class="th_deg">Product Quantity</th>
+                        <th class="th_deg">Price</th>
+                        <th class="th_deg">Image</th>
+                        <th class="th_deg">Action</th>
                     </tr>
 
-                    <?php $totalPrice = $totalPrice + $cart->price ?>
-                @endforeach
-            </table>
+                    <?php $totalPrice = 0; ?>
 
-            <div>
-                <h1 class="total_deg">Total Price: ${{$totalPrice}}</h1>
+                    @foreach ($cart as $cart)
+                        <tr>
+                            <td>{{$cart->product_title}}</td>
+                            <td>{{$cart->quantity}}</td>
+                            <td>${{$cart->price}}</td>
+                            <td><img class="img_deg" src="/product/{{$cart->image}}"></td>
+                            <td><a class="btn btn-danger" onclick="return confirm('Are you sure to remove this product?')" href="{{url('/remove_cart', $cart->id)}}">Remove</a></td>
+                        </tr>
+
+                        <?php $totalPrice = $totalPrice + $cart->price ?>
+                    @endforeach
+                </table>
+
+                <div>
+                    <h1 class="total_deg">Total Price: ${{$totalPrice}}</h1>
+                </div>
+
+                <div>
+                    <h1 style="font-size: 25px; padding-bottom: 15px">Proceed to Order</h1>
+                    <a href="{{url('cash_order')}}" class="btn btn-danger">Cash on Delivery</a>
+                    <a href="" class="btn btn-danger">Pay Using Card</a>
+                </div>
+
             </div>
 
+            @include('home.footer')
+            <!-- footer end -->
+            <div class="cpy_">
+                <p class="mx-auto">© 2021 All Rights Reserved By <a href="https://html.design/">Free Html Templates</a><br>
+                
+                    Distributed By <a href="https://themewagon.com/" target="_blank">ThemeWagon</a>
+                
+                </p>
+            </div>
+            <!-- jQery -->
+            <script src="home/js/jquery-3.4.1.min.js"></script>
+            <!-- popper js -->
+            <script src="home/js/popper.min.js"></script>
+            <!-- bootstrap js -->
+            <script src="home/js/bootstrap.js"></script>
+            <!-- custom js -->
+            <script src="home/js/custom.js"></script>
         </div>
-
-        @include('home.footer')
-        <!-- footer end -->
-        <div class="cpy_">
-            <p class="mx-auto">© 2021 All Rights Reserved By <a href="https://html.design/">Free Html Templates</a><br>
-            
-                Distributed By <a href="https://themewagon.com/" target="_blank">ThemeWagon</a>
-            
-            </p>
-        </div>
-        <!-- jQery -->
-        <script src="home/js/jquery-3.4.1.min.js"></script>
-        <!-- popper js -->
-        <script src="home/js/popper.min.js"></script>
-        <!-- bootstrap js -->
-        <script src="home/js/bootstrap.js"></script>
-        <!-- custom js -->
-        <script src="home/js/custom.js"></script>
    </body>
 </html>
