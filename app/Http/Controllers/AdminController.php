90db775c74d24ea1421cb449aea047373c53a12d
Md Masuk Al Hussain <alhussain@iut-dhaka.edu>
2024-08-13 17:59:41 +0600

    delivery status admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -98,4 +98,12 @@ class AdminController extends Controller
         $order = order::all();
         return view('admin.order', compact('order'));
     }
+
+    public function delivered($id){
+        $order = order::find($id);
+        $order->delivery_status = "Delivered";
+        $order->payment_status = "Paid";
+        $order->save();
+        return redirect()->back();
+    }
 }
