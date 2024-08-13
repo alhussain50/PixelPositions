d54047c71547f7c566e022aede8f6cffe7fd7b2c
Md Masuk Al Hussain <alhussain@iut-dhaka.edu>
2024-08-13 19:35:17 +0600

    email verification

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::middleware([
     })->name('dashboard');
 });
 
-route::get('redirect', [HomeController::class, 'redirect']);
+route::get('redirect', [HomeController::class, 'redirect'])->middleware('auth', 'verified');
 
 // Routes for Admin
 
